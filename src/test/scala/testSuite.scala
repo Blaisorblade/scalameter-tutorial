0fd4e7d6101fdf42e800c159c2bdda1d80161acc
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-09-14 16:02:46 +0200

    Example of performance-test suite from guide
    From
    http://axel22.github.io/scalameter/home/gettingstarted/configuration/index.html,
    with compile fixes. This requires ScalaMeter 0.4 since it measures
    memory footprint.

---
+++ src/test/scala/testSuite.scala
@@ -0,0 +1,46 @@
+import org.scalameter.api._
+
+class RegressionTest extends PerformanceTest.Regression {
+  def persistor = new SerializationPersistor
+
+  val sizes = Gen.range("size")(1000000, 5000000, 2000000)
+  val arrays = for (sz <- sizes) yield (0 until sz).toArray
+
+  performance of "Array" in {
+    measure method "foreach" in {
+      using(arrays) config (
+        exec.independentSamples -> 6
+      ) in { xs =>
+        var sum = 0
+        xs.foreach(x => sum += x)
+      }
+    }
+  }
+}
+
+class MemoryTest extends PerformanceTest.Regression {
+  def persistor = new SerializationPersistor
+  override def measurer = new Executor.Measurer.MemoryFootprint
+
+  val sizes = Gen.range("size")(1000000, 5000000, 2000000)
+
+  performance of "MemoryFootprint" in {
+    performance of "Array" in {
+      using(sizes) config (
+        exec.benchRuns -> 10,
+        exec.independentSamples -> 2
+      ) in { sz =>
+        (0 until sz).toArray
+      }
+    }
+  }
+}
+
+class TestSuite extends PerformanceTest.Regression {
+  //This overrides the persistor from the included classes.
+  //The same would happen for reporter.
+  def persistor = Persistor.None
+
+  include[MemoryTest]
+  include[RegressionTest]
+}
