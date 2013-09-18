3c67ea151263eb1dd5635d752a6c46816831b233
Paolo Giarrusso <p.giarrusso@gmail.com>
2013-09-18 21:55:30 +0200

    Get Dsv output, workarounding issue axel22#23
    Use the infrastructure for performance regression testing, to use the
    persistor.
    Disable performance regression testing by using Accepter as
    RegressionReporter.Tester.

--- src/test/scala/RangeBenchmark.scala
+++ src/test/scala/RangeBenchmark.scala
@@ -1,22 +1,47 @@
+import org.scalameter.{reporting, api, execution}
 import org.scalameter.api._
 
+//XXX: this uses def, not lazy vals, because of the code it's copied from. But this seems fishy.
+trait MyRegression extends PerformanceTest {
+  import Executor.Measurer
+  import reporting._
+  def warmer = Executor.Warmer.Default()
+  def aggregator = Aggregator.complete(Aggregator.average)
+  def measurer: Measurer = new Measurer.IgnoringGC with Measurer.PeriodicReinstantiation with Measurer.OutlierElimination with Measurer.RelativeNoise
+  def executor: Executor = new execution.SeparateJvmsExecutor(warmer, aggregator, measurer)
+  def regressionTester: RegressionReporter.Tester = RegressionReporter.Tester.OverlapIntervals()
+  def historian: RegressionReporter.Historian = RegressionReporter.Historian.ExponentialBackoff()
+  def regressionReporter =
+    new RegressionReporter(regressionTester, historian)
+  def reporter: Reporter = Reporter.Composite(
+    regressionReporter,
+    HtmlReporter(false)
+  )
+}
+
 object RangeBenchmark
-extends PerformanceTest {
+extends MyRegression {
+  override def regressionTester = RegressionReporter.Tester.Accepter()
+  /*
   //lazy val executor = SeparateJvmsExecutor(
   lazy val executor = LocalExecutor(
     new Executor.Warmer.Default,
     Aggregator.complete(Aggregator.min),
     new Measurer.Default)
+    */
 
-  lazy val reporter =
+  override lazy val reporter =
       Reporter.Composite(
         LoggingReporter()
+        , regressionReporter
         , DsvReporter(delimiter=';')
         , HtmlReporter(true)
         /*ChartReporter(ChartFactory.XYLine())*/
       )
 
-  lazy val persistor = Persistor.None
+  //Ouch!
+  //lazy val persistor = Persistor.None
+  lazy val persistor = new SerializationPersistor
 
   // multiple tests can be specified here
   val mult = 1
@@ -29,8 +54,8 @@ extends PerformanceTest {
 
   performance of "Range" in {
     measure method "map" config (
-      exec.benchRuns -> 10,
-      reports.regression.significance -> 0.05
+      exec.benchRuns -> 10
+      , reports.regression.significance -> 0.05
     ) in {
       using (ranges) in {
         r => r map (_ + 1)
